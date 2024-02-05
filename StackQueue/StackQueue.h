09245d500f3513e03946f75c607da1d89ee0543c
YJ\YJ_1223 <YJ2386708206@163.com>
2024-02-05 13:55:56 +0800

    2024.2.4 栈和队列 over~

--- StackQueue/StackQueue.h
+++ StackQueue/StackQueue.h
@@ -29,7 +29,6 @@ private:
             if (!que.empty() && value==que.front())
                 que.pop_front();
         }
-
         // 如果push的数值大于入口元素的数值，那么就将队列后端的数值弹出，直到push的数值小于等于队列入口元素的数值为止。
         // 这样就保持了队列里的数值是单调从大到小的了。
         void push(int value) {
@@ -37,7 +36,6 @@ private:
                 que.pop_back();
             que.push_back(value);
         }
-
         // 查询当前队列里的最大值 直接返回队列前端也就是front就可以了。
         int front() {
             return que.front();
